bd308554f6bef20d19368b2c5b89f7edb16eb416
maria luiza <151561030+maluviieira@users.noreply.github.com>
2026-03-07 17:06:01 +0000

    added hero animation

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,11 +1,32 @@
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 function Hero(){
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const nameRef = useRef<HTMLHeadingElement>(null);
+    const subtitleRef = useRef<HTMLParagraphElement>(null);
+    
+    useGSAP(() => {
+        const tl = gsap.timeline({delay: 0.2});
+
+        tl.from([nameRef.current, subtitleRef.current], {
+            y: 40,
+            opacity: 0,
+            duration: 1.2,
+            stagger: 0.2,
+            ease: "power3.out"
+        });
+    }, {scope : containerRef});
+
     return (
-        <div className="min-h-screen w-full flex items-end px-6 md:px-20 pb-20 md:pb-32">
+        <div ref={containerRef} className="min-h-screen w-full flex items-end px-6 md:px-20 pb-20 md:pb-32">
         <div className="flex flex-col gap-y-2">
-            <h1 className="text-6xl md:text-9xl font-semibold tracking-tighter leading-none">
+            <h1 ref={nameRef} className="text-6xl md:text-9xl font-semibold tracking-tighter leading-none">
                 malu vieira
             </h1>
-            <p className="text-lg md:text-2xl font-normal ml-1">
+            <p ref={subtitleRef} className="text-lg md:text-2xl font-normal ml-1">
                 computer engineering student, frontend dev
             </p>
         </div>
